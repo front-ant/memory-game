ff982b34869ffd453314d1acf468cbc57c498bee
Antje Lemberg <a.h.m.lemberg@gmail.com>
2018-05-26 10:49:16 +0200

    Add timer function

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ let openCards = [];
 let matchedCards = [];
 let moveCounter = document.getElementById('moves').innerText;
 let moves = parseInt(moveCounter);
+let time = 0;
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -74,9 +75,9 @@ for (card of cards) {
 //only fires after a match is checked so it won't track every click
  function increaseMoveCounter () {
    moves += 1;
-   document.getElementById('moves').innerText = moves + " Moves";
+   document.getElementById('moves').innerText = moves + ' Moves';
    if (moves === 1) {//This is petty but I like it
-     document.getElementById('moves').innerText = moves + " Move";
+     document.getElementById('moves').innerText = moves + ' Move';
    };
    if (moves === 18) {//star rating goes down with increasing moves, might be better in a separate function
      stars.removeChild(stars.querySelector('li'));
@@ -89,8 +90,14 @@ for (card of cards) {
    };
  };
 
-//This might as well be a startGame function or something like that...
-deck.addEventListener('click', function(event) {
+window.onload = function startGame() {
+  let timerVar = setInterval(increaseTimer, 1000);
+  function increaseTimer() {
+    time += 1;
+    timer.innerHTML = 'Time: ' + time;
+  }
+
+  deck.addEventListener('click', function(event) {
   if (event.target.nodeName === 'LI'//only fires if a card is clicked
   && openCards.length < 2//not more than two cards should be open at a time
   && (!event.target.classList.contains('open')//will not fire if the card is already open or matched
@@ -102,8 +109,9 @@ deck.addEventListener('click', function(event) {
       checkMatch();
       increaseMoveCounter();
     };
-  if (matchedCards.length === 16) {//end of game
-    alert('Congrats! You finished the game in ' + moves + ' moves!');
+  if (matchedCards.length === 2) {//end of game
+    alert('Congrats! You finished the game in ' + moves + ' moves and ' + time + ' seconds!');
   };
 }
 });
+}
